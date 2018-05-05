df633b9483b3bbcede0e688add5504d19182377b
Guillaume Langlois <g-langlois@users.noreply.github.com>
2018-05-04 20:05:41 -0700

    Saves the selected isotope to user defaults

--- DecayCorrect/State.swift
+++ DecayCorrect/State.swift
@@ -9,11 +9,17 @@
 import Foundation
 class State {
     
+    let defaults = UserDefaults.standard
     var isotopes = [Isotope]()
-    var selectedIsotopeIndex: Int?
+    var selectedIsotopeIndex: Int? {
+        willSet {
+            defaults.set(newValue, forKey: "isotopeIndex")
+        }
+    }
     init() {
         isotopes.append(Isotope(atomName: "Fluoride", atomSymbol: "F", halfLife: TimeInterval(110*60), massNumber: 18))
         isotopes.append(Isotope(atomName: "Gallium", atomSymbol: "Ga", halfLife: TimeInterval(68*60), massNumber: 68))
+        selectedIsotopeIndex = defaults.integer(forKey: "isotopeIndex") 
 
     }
 }
